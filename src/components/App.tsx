7754e9c86afbeb77e74027e112995a24c6cf240a
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-25 18:45:44 +0000

    type app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,7 @@
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { FC } from 'react';
+
+const App: FC = (): JSX.Element => {
+  return <div className="App">REact app</div>;
+};
 
 export default App;

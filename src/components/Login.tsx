d9710bbff12f9ee6aaa162e830a3166c906d5590
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-27 18:09:32 +0000

    login state interface

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import AuthService from '../services/AuthService';
 
-interface LoginState {
+interface LoginProps {
   authService: AuthService;
 }
 
+interface LoginState {
+  userName: string;
+  password: string;
+  loginTried: boolean;
+  loginSuccess: boolean;
+}
+
 // eslint-disable-next-line react/prefer-stateless-function
-class Login extends React.Component<LoginState> {
+class Login extends React.Component<LoginProps, LoginState> {
   render(): JSX.Element {
     return <div>Login</div>;
   }

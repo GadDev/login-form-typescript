8ee02f28ca2764ed48667f529a6a45731d653416
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-29 18:04:21 +0100

    implement link and loginLink logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,24 @@
 import { User } from 'model/Model';
 import { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 // eslint-disable-next-line react/prefer-stateless-function
 class Navbar extends Component<{ user: User }> {
   render(): JSX.Element {
-    return <div>Welcome to your home page</div>;
+    const { user } = this.props;
+    let loginLink: JSX.Element;
+    if (user) {
+      loginLink = <Link to="/logout">{user.userName}</Link>;
+    } else {
+      loginLink = <Link to="/login">Login</Link>;
+    }
+
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        {loginLink}
+      </div>
+    );
   }
 }
 

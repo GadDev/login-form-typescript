03dbb302bc269c4b9c15fd177dc7c50271b42c47
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-29 23:02:25 +0100

    modify route render Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,17 @@
 import { Component } from 'react';
 
+import { User } from '../model/Model';
+import { AuthService } from '../services/AuthService';
+
+interface ProfileState {
+  userAttributes: UserAttributes[];
+}
+interface ProfileProps {
+  user: User | undefined;
+  authService: AuthService;
+}
 // eslint-disable-next-line react/prefer-stateless-function
-class Profile extends Component {
+class Profile extends Component<ProfileProps, ProfileState> {
   render(): JSX.Element {
     return <div>Welcome to your profile page</div>;
   }

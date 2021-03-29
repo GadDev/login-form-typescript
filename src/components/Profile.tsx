30b71e5c76dc27d1d100fadbb1af662e125c4ec2
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-29 17:56:10 +0100

    profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Profile extends Component {
+  render(): JSX.Element {
+    return <div>Welcome to your profile page</div>;
+  }
+}
+
+export default Profile;

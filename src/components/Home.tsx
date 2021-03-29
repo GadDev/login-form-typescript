d6ce9cbca9be97ef613e75b35358b986f4cf5937
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-29 17:57:04 +0100

    home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Home extends Component {
+  render(): JSX.Element {
+    return <div>Welcome to your profile page</div>;
+  }
+}
+
+export default Home;
